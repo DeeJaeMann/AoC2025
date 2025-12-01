c24082c8b09f57aed1bf02904392b789b8dc52c3
Dee J. Mann <dee.j.mann@hotmail.com>
2025-12-01 11:56:35 -0500

    Added part 1 Solution

---
+++ Day1_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<string> input = File.ReadAllLines("input.txt").ToList();
+int count = 0;
+int position = 50;
+
+foreach (string line in input)
+{
+    int amount = 0;
+    if (line.StartsWith("L"))
+    {
+        amount = int.Parse(line.Split('L')[1]);
+        position -= amount;
+    }
+    else
+    {
+        amount = int.Parse(line.Split('R')[1]);
+        position += amount;
+    }
+
+    position %= 100;
+
+    if (position is 0)
+    {
+        count++;
+    }
+
+}
+Console.WriteLine($"Final count is {count}");
