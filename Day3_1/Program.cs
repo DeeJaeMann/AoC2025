c06ad68a00aaad94025c865e29d6cb1e45f0908b
Dee J. Mann <dee.j.mann@hotmail.com>
2025-12-03 09:31:17 -0500

    Added Day 3 Part 1

---
+++ Day3_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+List<string> input =  File.ReadAllLines("input.txt").ToList();
+List<int> maxJolts = new();
+
+foreach (string line in input)
+{
+    #if DEBUG
+    Debug.WriteLine(line);
+    #endif
+    var indexedDigits = line
+        .Select((digit, index) => new { Value = int.Parse(digit.ToString()), Index = int.Parse(index.ToString()) });
+    #if DEBUG
+    foreach (var element in indexedDigits)
+    {
+        Debug.WriteLine(element);
+    }
+    #endif
+    var firstMax = indexedDigits
+        .OrderByDescending(digit => digit.Value)
+        .First();
+    var secondMax = indexedDigits
+        .Where(digit => digit.Index > firstMax.Index)
+        .OrderByDescending(digit => digit.Value)
+        .FirstOrDefault();
+    Debug.WriteLine($"firstMax: {firstMax}");
+    Debug.WriteLine($"secondMax: {secondMax}");
+    if (secondMax is null)
+    {
+        secondMax = firstMax;
+        firstMax = indexedDigits
+            .Where(digit => digit.Index < firstMax.Index)
+            .OrderByDescending(digit => digit.Value)
+            .FirstOrDefault();
+        Debug.WriteLine("secondMax empty");
+        Debug.WriteLine($"New firstMax: {firstMax}");
+        Debug.WriteLine($"New secondMax: {secondMax}");
+    }
+    maxJolts.Add(int.Parse($"{firstMax.Value}{secondMax.Value}"));
+}
+
+#if DEBUG
+foreach (int num in maxJolts)
+{
+    Debug.WriteLine(num);
+}
+#endif
+
+Console.WriteLine($"Number of Jolts: {maxJolts.Count}");
+Console.WriteLine($"Max Jolts: {maxJolts.Sum()}");
